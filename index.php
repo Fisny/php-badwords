c215319b96142717ec848a4cecfcaea64941d7a1
Fisny <bardi.fisniku@gmail.com>
2021-08-26 09:00:41 +0200

    9:00

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<!--  -->
+<?php
+    $var = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quo consequatur in, laborum odio ea quam magnam perferendis. Vitae, odio suscipit eligendi at corporis vero esse aspernatur reprehenderit ut fugit!";
+    $find = $_GET = ["lorem"];
+    // $result = stripos($var, $find);
+
+    if ($find !== false ) {
+        echo "(***) {$var}";
+    }
+?>
+<!--  -->
+</head>
+<body>
+    
+</body>
+</html>
